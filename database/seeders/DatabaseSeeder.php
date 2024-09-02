085c3031e641b922c513184d75cacc1bc6082b95
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2024-09-02 12:09:48 +0100

    cleanup: allow updating password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,15 +19,27 @@ class DatabaseSeeder extends Seeder
         $adminUserEmail = env('XTREAM_ADMIN_USER_EMAIL');
         $adminUserPassword = env('XTREAM_ADMIN_USER_PASSWORD');
 
-        if (!$adminUserName || !$adminUserEmail || !$adminUserPassword) {
+        if(!$adminUserName || !$adminUserEmail || !$adminUserPassword)
+        {
             throw new \Exception('Required environment variables XTREAM_ADMIN_USER_NAME, XTREAM_ADMIN_USER_EMAIL, and XTREAM_ADMIN_USER_PASSWORD are missing.');
         }
 
-        // Create the user with the environment variable values
-        User::create([
-            'name' => $adminUserName,
-            'email' => $adminUserEmail,
-            'password' => Hash::make($adminUserPassword),
-        ]);
+        if(!User::where('email', $adminUserEmail)->exists())
+        {
+            // Create the user if it does not exist
+            User::create([
+                'name' => env('XTREAM_ADMIN_USER_NAME'),
+                'email' => $adminUserEmail,
+                'password' => Hash::make(env('XTREAM_ADMIN_USER_PASSWORD')),
+            ]);
+        }
+        else
+        {
+            // Optionally, update the existing user details
+            User::where('email', $adminUserEmail)->update([
+                'password' => Hash::make(env('XTREAM_ADMIN_USER_PASSWORD')),
+            ]);
+        }
+
     }
 }
