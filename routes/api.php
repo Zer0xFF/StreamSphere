a27cbcea4192f278545bacbc233b07386a5155df
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2024-09-02 12:59:35 +0100

    add /refreshfilters to middleware provider

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::middleware([ProviderMiddleware::class])->group(function () {
     Route::get('/player_api.php', [XtreamCodeController::class, 'PlayerApi']);
     Route::get('/xmltv.php', [XtreamCodeController::class, 'getEPG']);
     Route::get('/live/{username}/{password}/{filename}', [XtreamCodeController::class, 'redirectToExternal']);
+    Route::get('/refreshfilters', [XtreamCodeController::class, 'refreshFilters']);
 });
 
 Route::get('/clearcache', [XtreamCodeController::class, 'clearCache']);
-Route::get('/refreshfilters', [XtreamCodeController::class, 'refreshFilters']);
